1215a9a78bb152dfd01a39538fd173668a34375c
Adorilson Bezerra <adorilson@gmail.com>
2020-01-16 19:32:40 -0300

    Cleaning src/App.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,8 @@
-import React, {useState} from 'react';
-
-
-// Componente: bloco isolado de HTML, CSS e JS o qual nao interfere
-// no resto da aplicação 
-// Estado: informações mantida pelo componente (lembrar: imutabilidade)
-// Propriedade:  Informações que um componente PAI passa para o componente FILHO
-
-
+import React from 'react';
 
 function App() {
-
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter(){
-    setCounter(counter + 1);
-  }
-
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <></>
     );
 }
 
