5330bc2dbdd87b6b644f041de88d73180f751801
Adorilson Bezerra <adorilson@gmail.com>
2020-01-18 12:34:02 -0300

    [mobile] Added a callout to show dev info in map

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image } from 'react-native';
-import MapView, { Marker }  from 'react-native-maps';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout }  from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main(){
@@ -38,6 +38,13 @@ function Main(){
                     source={{ uri:'https://avatars0.githubusercontent.com/u/169837?s=460&v=4'}}
                     style={styles.avatar}
                 />
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Adorilson Bezerra</Text>
+                        <Text style={styles.devBio}>professor</Text>
+                        <Text style={styles.devTechs}>Python, Linux</Text>
+                    </View>
+                </Callout>
             </Marker>
         </MapView>
     )
@@ -53,6 +60,20 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         borderWidth: 2,
         borderColor: '#FFF',
+    },
+    callout: {
+        width: 260,
+    },
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+    devTechs: {
+        marginTop: 5,
     }
 })
 
