786250f011971886624580c6c872407c3333d089
Adorilson Bezerra <adorilson@gmail.com>
2020-01-18 10:24:06 -0300

    [mobile] Using styles from a StyleSheet object instead of inline

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import MapView  from 'react-native-maps';
 
 function Main(){
-    return <MapView style={{flex: 1}}/>
+    return <MapView style={styles.map}/>
 }
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    }
+})
 
 export default Main;
