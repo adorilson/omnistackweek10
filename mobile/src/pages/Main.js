dc603bf3598f6ee80a8539d4cb9f5b9bf0b25d95
Adorilson Bezerra <adorilson@gmail.com>
2020-01-18 12:04:57 -0300

    [mobile] Showing a marker with avatar in map

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView  from 'react-native-maps';
+import { StyleSheet, Image } from 'react-native';
+import MapView, { Marker }  from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main(){
@@ -31,12 +31,28 @@ function Main(){
         return null;
     }
 
-    return <MapView initialRegion={currentRegion} style={styles.map}/>
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{latitude: -5.653684, longitude: -35.4277428}}>
+                <Image
+                    source={{ uri:'https://avatars0.githubusercontent.com/u/169837?s=460&v=4'}}
+                    style={styles.avatar}
+                />
+            </Marker>
+        </MapView>
+    )
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1
+    },
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 2,
+        borderColor: '#FFF',
     }
 })
 
